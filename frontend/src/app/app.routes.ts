3d457878fb3955de4f0dde203d879f90236579cc
Cuchillon <ferickadmin@gmail.com>
2025-01-05 13:26:57 +0300

    Fixing styles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './components/modules/login-form/login-form.component';
+import { HomeComponent } from './components/modules/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'login-form',
     component: LoginFormComponent
